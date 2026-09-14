76bf761d9384570d9326e367925012b595203719
Roopanshi-Marwaha <roopanshimarwaha@gmail.com>
2026-09-14 23:50:09 +0530

    made object of flask

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-from flask import flask
+from flask import Flask
+
+app=Flask(__name__)
+#making obj of flask
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+if __name__=='__main__':
+    app.run(debug=True)
